7fdf67beb142996c7f2d4863f61de0b7fc865682
Nope79 <nopesep@gmail.com>
2025-11-03 17:12:25 -0600

    Primer commit de los yharnamitas

---
+++ Yharnam-Task/Yharnam-Task/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Yharnam_Task
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Yharnam-Task/Yharnam-Task/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Yharnam_Task
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
