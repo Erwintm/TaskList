5db3eca4793e4bfd0192858b0a6779dbb14aa3f4
Nope79 <nopesep@gmail.com>
2025-11-03 18:06:25 -0600

    2do commit

--- Yharnam-Task/Yharnam-Task/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/Yharnam-Task.AssemblyInfo.cs
+++ Yharnam-Task/Yharnam-Task/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/Yharnam-Task.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Yharnam-Task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7fdf67beb142996c7f2d4863f61de0b7fc865682")]
 [assembly: System.Reflection.AssemblyProductAttribute("Yharnam-Task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Yharnam-Task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
